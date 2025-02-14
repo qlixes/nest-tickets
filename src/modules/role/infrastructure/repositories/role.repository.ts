3186bae7bffbe3279f74b3fdba55aa50938d89b9
Tuntaz Dev 140 <qlixess@gmail.com>
2025-02-14 17:58:02 +0700

    feat: update role repository

--- src/modules/role/infrastructure/repositories/role.repository.ts
+++ src/modules/role/infrastructure/repositories/role.repository.ts
@@ -1,24 +1,50 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
-import { Role } from "@prisma/client";
-import { PrismaService } from "src/common/prisma/prisma.service";
+import { Injectable } from '@nestjs/common';
+import { Role } from '@prisma/client';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
 @Injectable()
 export class RoleRepository {
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-    async findId(id: number): Promise<Role> {
-        let role = await this.prisma.role.findFirst({
-            where: {
-                id: id,
-            },
-        });
+  async findOne(data: any): Promise<Role> {
+    const role = await this.prisma.role.findFirst({
+      where: data,
+    });
 
-        return role;
-    }
+    return role;
+  }
 
-    async show(): Promise<Role[]> {
-        let role = await this.prisma.role.findMany();
+  /**
+   * property: {
+   *    page: number,
+   *    limit: number,
+   *    total: number,
+   *    pages: number,
+   *    sort: string,
+   * }
+   */
+  async findMany(data: any, property: any): Promise<Role[]> {
+    const role = await this.prisma.role.findMany({
+      where: data,
+    });
 
-        return role;
-    }
+    return role;
+  }
+
+  async store(data: any): Promise<Role> {
+    const role = await this.prisma.role.create({
+      data: data,
+    });
+
+    return role;
+  }
+
+  async update(condition: any, data: any): Promise<Role> {
+    const role = await this.prisma.role.update({
+      where: condition,
+      data: data,
+    });
+
+    return role;
+  }
 }
