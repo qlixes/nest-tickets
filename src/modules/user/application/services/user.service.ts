1edb9784c2c09d6e97c4e41a8922ad04ac711177
Tuntaz Dev 140 <qlixess@gmail.com>
2025-02-14 16:52:52 +0700

    fix: fix linter in user.service

--- src/modules/user/application/services/user.service.ts
+++ src/modules/user/application/services/user.service.ts
@@ -1,33 +1,32 @@
-import { Injectable } from "@nestjs/common";
-import { UserRepository } from "../../infrastructure/repositories/user.repository";
-import { UserEntity } from "../../domain/entities/user.entity";
-import { DuplicateDataException } from "src/common/exceptions/duplicate-data.exception";
+import { Injectable } from '@nestjs/common';
+import { UserRepository } from '../../infrastructure/repositories/user.repository';
+import { DuplicateDataException } from 'src/common/exceptions/duplicate-data.exception';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-    constructor(private readonly repository: UserRepository) {}
+  constructor(private readonly repository: UserRepository) {}
 
-    async signup(params: any) {
-        let user = await this.repository.find(params.email);
+  async signup(params: any) {
+    const user = await this.repository.find(params.email);
 
-        if(user) {
-            throw new DuplicateDataException();
-        }
-
-        let password = await bcrypt.hash(params.password, 16);
-
-        let data = {
-            roleId: params.role_id,
-            name: params.name,
-            email: params.email,
-            phone: params.phone,
-            password: password,
-            telegramId: params.telegram_id,  
-        };
-
-        let store = await this.repository.create(data);
-
-        return store;
+    if (user) {
+      throw new DuplicateDataException();
     }
+
+    const password = await bcrypt.hash(params.password, 16);
+
+    const data = {
+      roleId: params.role_id,
+      name: params.name,
+      email: params.email,
+      phone: params.phone,
+      password: password,
+      telegramId: params.telegram_id,
+    };
+
+    const store = await this.repository.create(data);
+
+    return store;
+  }
 }
