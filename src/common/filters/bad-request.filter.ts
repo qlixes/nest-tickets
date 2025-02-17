ccac89cfcd1b0db90a21d2828aab04dee967f6c3
Tuntaz Dev 140 <qlixess@gmail.com>
2025-02-17 16:51:22 +0700

    fix: fix class name for backward

--- src/common/filters/bad-request.filter.ts
+++ src/common/filters/bad-request.filter.ts
@@ -1,8 +1,8 @@
 import { BadRequestException, Catch, ExceptionFilter, ArgumentsHost } from "@nestjs/common";
 import { Request, Response } from 'express';
 
-// @Catch(BadRequestException)
-export class BadRequestFilter implements ExceptionFilter {
+@Catch(BadRequestException)
+export class BadRequest implements ExceptionFilter {
     catch(exception: BadRequestException, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
