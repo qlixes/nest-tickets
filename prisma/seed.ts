7d20a2423371c65d293b9343ee18730b358df14d
Tuntaz Dev 140 <qlixess@gmail.com>
2025-02-17 16:52:07 +0700

    feat: add index and seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,33 +1,35 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 async function main() {
   const developer = await prisma.role.create({
     data: {
-      name: "developer",
+      name: 'developer',
     },
   });
 
   const admin = await prisma.role.create({
     data: {
-      name: "admin",
+      name: 'admin',
     },
   });
 
   const merchant = await prisma.role.create({
     data: {
-      name: "merchant",
+      name: 'merchant',
     },
   });
+
+  console.log({ developer, admin, merchant });
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
