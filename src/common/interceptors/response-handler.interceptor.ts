c9baaf0ac66bca404adfd3afd1eeea0078674815
Tuntaz Dev 140 <qlixess@gmail.com>
2025-02-21 14:20:55 +0700

    refactor: add clean code
    * comment console message
    * todo: console message kept show undefined

--- src/common/interceptors/response-handler.interceptor.ts
+++ src/common/interceptors/response-handler.interceptor.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, CallHandler, ExecutionContext, HttpException, HttpStatus, Injectable, NestInterceptor } from "@nestjs/common";
+import { CallHandler, ExecutionContext, HttpException, HttpStatus, Injectable, NestInterceptor } from "@nestjs/common";
 import { Observable, throwError } from "rxjs";
 import { catchError, map } from 'rxjs/operators';
 
@@ -22,24 +22,40 @@ export class ResponseHandlerInterceptor<T> implements NestInterceptor<T, Respons
     }
 
     errorHandler(exception: HttpException, context: ExecutionContext) {
+
         const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
 
         const ctx = context.switchToHttp();
         const response = ctx.getResponse();
 
-        response.status(status).json({
+        const format = {
             code: "111",
             message: exception.message,
-        });
+        };
+
+        // response
+        response.status(status).json(format);
+
+        // show in console
+        // return format.message;
+        // return exception.message;
     }
 
     responseHandler(res: any, context: ExecutionContext) {
-        const status =  "000";
 
-        return {
+        const ctx = context.switchToHttp();
+        const response = ctx.getResponse();
+
+        const format = {
             code: "000",
             message: "Succesfully",
             data: res,
         };
+
+        // response
+        response.status(HttpStatus.ACCEPTED).json(format);
+
+        // show in console
+        // return format.message;
     }
 }
