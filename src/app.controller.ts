d2abd254fccbd34820aba5040a8d40e4759ea275
Tuntaz Dev 140 <qlixess@gmail.com>
2025-02-27 17:11:02 +0700

    clean: remove any endpoint in app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,31 +1,5 @@
 import { Body, Controller, Post, UseFilters } from '@nestjs/common';
-import { UserService } from './modules/user/application/services/user.service';
-import { BadRequest } from './common/filters/bad-request.filter';
-import { CreateUserDto } from './modules/user/application/dto/create-user.dto';
 
 @Controller()
 export class AppController {
-  // constructor(
-  //   private readonly user: UserService,
-  // ) {}
-
-  // @Post('user/login')
-  // @UseFilters(BadRequest)
-  // async getUser() {
-  //   return this.user.show();
-  // }
-
-  // @Post('user/list')
-  // async getUsers() {}
-
-  // @Post('user/store')
-  // async postUser(@Body() dto: CreateUserDto) {
-  //   return this.user.store(dto);
-  // }
-
-  // @Post('user/update')
-  // async patchUser() {}
-
-  // @Post('user/remove')
-  // async deleteUser() {}
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { PrismaModule } from './common/prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, UserModule, AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
