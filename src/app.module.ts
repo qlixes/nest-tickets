0c25278b97bd60239418b5f3a1e6802ba9c2f425
Tuntaz Dev 140 <qlixess@gmail.com>
2025-02-28 18:07:28 +0700

    feat: add custom service
    * add redis service
    * add auth-code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './common/prisma/prisma.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { RedisModule } from 'nestjs-redis';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [PrismaModule, UserModule, AuthModule, RedisModule, CacheModule.registerAsync({}), CacheModule.register({ isGlobal: true})],
   controllers: [],
   providers: [],
 })
